a58086baefdaae16e31723bbeb8363deafcc5869
Luckyantony <luckyantonyofficial@gmail.com>
2025-07-19 23:46:25 +0300

    Updated models resource file

---
+++ app/models/resource.py
@@ -0,0 +1,6 @@
+from app.extensions import db
+
+class Resource(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
