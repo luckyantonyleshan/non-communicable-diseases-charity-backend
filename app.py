272b3439800a489691a824ede1a2b335e6245864
Luckyantony <luckyantonyofficial@gmail.com>
2025-07-19 23:40:10 +0300

    updated app.py file and init for models folder

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from app import create_app
+from flask import Flask
+from app.extensions import db, migrate, jwt
+from app.config import Config
+from app.routes.auth_routes import auth_bp
+from app.routes.user_routes import user_bp
+from app.routes.case_routes import case_bp
+from app.routes.donation_routes import donation_bp
+from app.routes.resource_routes import resource_bp
 
-app = create_app()
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-if __name__ == '__main__':
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(user_bp, url_prefix='/users')
+    app.register_blueprint(case_bp, url_prefix='/cases')
+    app.register_blueprint(donation_bp, url_prefix='/donations')
+    app.register_blueprint(resource_bp, url_prefix='/resources')
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from .user import User
+from .case import Case
+from .donation import Donation
+from .resource import Resource
+from .association import donation_case
