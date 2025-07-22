d59eac29af4722deba5e7757d798788a53cc7e43
Luckyantony <luckyantonyofficial@gmail.com>
2025-07-22 20:00:01 +0300

    debugging code

--- app/config.py
+++ app/config.py
@@ -4,5 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', '').replace('postgres://', 'postgresql://')
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///instance/dev.db').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'fallback-jwt-key')
