77315e39297902ba1566af9a76ae44b14d47d207
Luckyantony <luckyantonyofficial@gmail.com>
2025-07-22 18:57:24 +0300

    Fix database migration for PostgreSQL

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
