e1219317b44d0e6a09734a2f78c16f2251fd8d11
Luckyantony <luckyantonyofficial@gmail.com>
2025-07-22 18:08:21 +0300

    More changes

--- app/config.py
+++ app/config.py
@@ -1,11 +1,5 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key-for-dev-only")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "default-jwt-secret-for-dev-only")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///instance/db.sqlite3")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    PROPAGATE_EXCEPTIONS = True
